34a5e9fcfd98750ccceba9fc5724e3ccc544282b
Rod-X <55778305+Rod-X@users.noreply.github.com>
2022-06-13 15:30:59 +0800

    butfferApi

--- node/Buffer-api.js
+++ node/Buffer-api.js
@@ -26,3 +26,24 @@ const buf5 = Buffer.from('tést');
 
 // 创建一个包含 Latin-1 字节 [0x74, 0xe9, 0x73, 0x74] 的 Buffer。
 const buf6 = Buffer.from('tést', 'latin1');
+
+
+// Buffer实例方法
+// buf.fill(value[, offset][, end])
+// 使用指定的 value 来填充这个 buffer。如果没有指定 offset (默认是 0) 并且 end (默认是 buffer.length) ，将会填充整个buffer。
+
+// buf.write(string[, offset[, length]][, encoding])
+// 根据参数 offset 偏移量和指定的 encoding 编码方式，将参数 string 数据写入buffer。 offset 偏移量默认值是 0, encoding 编码方式默认是 utf8。 length 长度是将要写入的字符串的 bytes 大小。 返回 number 类型，表示写入了多少 8 位字节流。如果 buffer 没有足够的空间来放整个 string，它将只会只写入部分字符串。 length 默认是 buffer.length - offset。 这个方法不会出现写入部分字符。
+
+// buf.toString([encoding[, start[, end]]])
+// 根据 encoding 参数（默认是 'utf8'）返回一个解码过的 string 类型。还会根据传入的参数 start (默认是 0) 和 end (默认是 buffer.length)作为取值范围。
+
+// buf.slice([start[, end]])
+// 剪切 Buffer 对象，根据 start(默认是 0 ) 和 end (默认是 buffer.length ) 偏移和裁剪了索引。 负的索引是从 buffer 尾部开始计算的。
+
+// buf.copy(targetBuffer[, targetStart[, sourceStart[, sourceEnd]]])
+// buffer 拷贝，源和目标可以相同。 targetStart 目标开始偏移和 sourceStart 源开始偏移默认都是 0。 sourceEnd 源结束位置偏移默认是源的长度 buffer.length 。
+
+// buf.indexOf(value[, byteOffset][, encoding])
+// 该操作类似于 Array#indexOf() ，它返回 value 在 Buffer 中的最开始的索引位置，如果当前 Buffer 不包含这个 value 则返回 -1 。这个 value 的值可以是 String 、Buffer 或 Number 。字符串会默认用 UTF8 解释执行。Buffer 将会使用整个 Buffer（比较部分 Buffer 请使用 buf.slice() 方法）。数字在 0 到 255 的范围内。
+
---
+++ node/node基础.md
@@ -0,0 +1,8 @@
+# node基础
+
+## 全局变量
+process 提供与当前进程互动的接口
+timer 执行顺序与事件循环间的关系
+modules export 模块导入导出
+__filename 获取当前执行脚本所在的绝对路径
+__dirname 获取当前执行脚本所在的目录
