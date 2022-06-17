6a5515973bb20f2446cb44a913744612cf6b12ed
Rod-X <55778305+Rod-X@users.noreply.github.com>
2022-06-17 11:43:52 +0800

    Event Bus

--- 算法手写解析/js基础手写题.js
+++ 算法手写解析/js基础手写题.js
@@ -184,3 +184,46 @@ _.throttle = function(func, wait, options) {
       return result;
     };
   };
+
+//   Event Bus
+class Events {
+    constructor() {
+      this.events = new Map();
+    }
+  
+    addEvent(key, fn, isOnce, ...args) {
+      const value = this.events.get(key) ? this.events.get(key) : this.events.set(key, new Map()).get(key)
+      value.set(fn, (...args1) => {
+          fn(...args, ...args1)
+          isOnce && this.off(key, fn)
+      })
+    }
+  
+    on(key, fn, ...args) {
+      if (!fn) {
+        console.error(`没有传入回调函数`);
+        return
+      }
+      this.addEvent(key, fn, false, ...args)
+    }
+  
+    fire(key, ...args) {
+      if (!this.events.get(key)) {
+        console.warn(`没有 ${key} 事件`);
+        return;
+      }
+      for (let [, cb] of this.events.get(key).entries()) {
+        cb(...args);
+      }
+    }
+  
+    off(key, fn) {
+      if (this.events.get(key)) {
+        this.events.get(key).delete(fn);
+      }
+    }
+  
+    once(key, fn, ...args) {
+      this.addEvent(key, fn, true, ...args)
+    }
+  }
